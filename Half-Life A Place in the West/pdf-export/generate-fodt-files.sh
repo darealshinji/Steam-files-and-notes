7801cd8b7181728008d4e2acd269cbf173faff1d
darealshinji <djcj@gmx.de>
2017-10-01 03:57:59 +0200

    Half-Life A Place in the West: update build script, delete pre-generated documents

--- Half-Life A Place in the West/pdf-export/generate-fodt-files.sh
+++ Half-Life A Place in the West/pdf-export/generate-fodt-files.sh
@@ -1,29 +1,53 @@
 #!/bin/bash
+set -e
 
-# Usage: gen_fodt path output
-function gen_fodt()
-{
-  local doc="$2"
-  cat "01-header.xml" "02-settings.xml" "03-body.xml" > $doc
-  n=1
-  for f in "$1"/*.jpg ; do
-    sed "s,@NUMBER@,$n,g; s,@IMAGE@,$f,g" "04-draw-template.xml" >> $doc
-    n=$(($n + 1))
-  done
-  cat "05-end.xml" >> $doc
+# generate OpenDocument files; use LibreOffice for manual PDF export
+
+apw_root="$HOME/.local/share/Steam/steamapps/common/Half-Life A Place in the West"
+
+TOP="$(cd "$(dirname "$0")" && pwd)"
+
+function gen_fodt() {
+  if [ -d "$1" ]; then
+    local doc="$2"
+    cat "$TOP/01-header.xml" "$TOP/02-settings.xml" "$TOP/03-body.xml" > $doc
+    n=1
+    for f in "$1"/*.jpg ; do
+      sed "s,@NUMBER@,$n,g; s,@IMAGE@,$f,g" "$TOP/04-draw-template.xml" >> $doc
+      n=$(($n + 1))
+    done
+    cat "$TOP/05-end.xml" >> $doc
+  fi
 }
 
-# Use asar to extract .asar files:
-# npm install asar
-# ./node_modules/.bin/asar extract issue2.asar issue_2
-# ./node_modules/.bin/asar extract issue3.asar issue_3
+function extract_asar() {
+  if [ -e "$1" ]; then
+    "$TOP/../asar-extract.sh" extract "$1" "$2" >/dev/null
+  fi
+}
 
-gen_fodt issue_1/EN issue-1-en.fodt
-gen_fodt issue_1/RU issue-1-ru.fodt
-gen_fodt issue_2/EN issue-2-en.fodt
-gen_fodt issue_2/RU issue-2-ru.fodt
-gen_fodt issue_3/EN issue-3-en.fodt
-gen_fodt issue_3/RU issue-3-ru.fodt
-gen_fodt issue_charnel/EN issue-charnel-en.fodt
-gen_fodt issue_charnel/RU issue-charnel-ru.fodt
+mkdir output
+cd output
+
+echo "extract archives"
+for n in 2 3 4; do
+  extract_asar "$apw_root/issue$n/issue${n}.asar" issue_$n
+done
+
+echo "copy files"
+dist="$apw_root/resources/app/dist/comic"
+cp -r "$dist/issue_1" .
+cp -r "$dist/issue_charnel" .
+cp -r "$dist/issue_nal" .
+
+echo "generate documents"
+issues=$(ls -d issue_*/ | sed 's|issue_||g; s|/||g')
+for i in $issues; do
+  locales=$(ls -d issue_$i/*/ 2>/dev/null | cut -d'/' -f2)
+  for l in $locales; do
+    gen_fodt issue_$i/$l issue-${i}-${l,,}.fodt
+  done
+done
+
+echo "done"
 
