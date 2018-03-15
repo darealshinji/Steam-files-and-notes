1be8868dfda9030513abd2e51e6dbf79195e2f17
darealshinji <djcj@gmx.de>
2018-03-15 20:54:38 +0100

    Black Mesa / win32 launcher: check registry for path to Steam.exe

--- Black Mesa/bms_win32.cpp
+++ Black Mesa/bms_win32.cpp
@@ -1,13 +1,70 @@
 // windres bms.res bms_res.o
 // g++ -m32 -Wall -O3 -mwindows bms_win32.cpp -o bms.exe bms_res.o -lshlwapi -s
 
-// cl /Ox /EHsc /GS /guard:cf bms_win32.cpp /link /out:bms.exe bms.res user32.lib shlwapi.lib
+// cl /Ox /EHsc /GS /guard:cf bms_win32.cpp /link /out:bms.exe bms.res user32.lib advapi32.lib shlwapi.lib
 
 #include <windows.h>
 #include <shlwapi.h>
 #include <stdlib.h>
+#include <string.h>
 #include <string>
 
+
+std::string get_steam_path(std::string rootdir)
+{
+  HKEY hKey;
+  DWORD size;
+  char data[1024] = {0};
+  char *s;
+  std::string str = "";
+
+  if (RegOpenKeyExA(HKEY_CLASSES_ROOT, "steam\\Shell\\Open\\Command", 0, KEY_READ, &hKey) != ERROR_SUCCESS) {
+    goto check_rootdir;
+  }
+
+  if (RegQueryValueExA(hKey, NULL, NULL, NULL, (LPBYTE)data, &size) != ERROR_SUCCESS) {
+    RegCloseKey(hKey);
+    goto check_rootdir;
+  }
+
+  if (data[0] == '"') {
+    s = strtok(data + 1, "\"");
+    if (s != NULL) {
+      str = std::string(s);
+    }
+  }
+
+  RegCloseKey(hKey);
+
+  if (str != "" && PathFileExists(str.c_str())) {
+    //MessageBox(0, "Path to Steam.exe found in registry", "Info", MB_OK);
+    return str;
+  }
+
+check_rootdir:
+
+  str = rootdir + "\\..\\..\\..\\Steam.exe";
+  if (PathFileExists(str.c_str())) {
+    return str;
+  }
+
+  str = "C:\\Program Files (x86)\\Steam\\Steam.exe";
+  if (PathFileExists(str.c_str())) {
+    return str;
+  }
+
+  str = "C:\\Program Files\\Steam\\Steam.exe";
+  if (PathFileExists(str.c_str())) {
+    return str;
+  }
+
+  str = "C:\\Steam\\Steam.exe";
+  if (PathFileExists(str.c_str())) {
+    return str;
+  }
+
+  return "";
+}
 
 int APIENTRY WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
 {
@@ -26,14 +83,12 @@ int APIENTRY WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLi
   LauncherMain_t pLauncherMain;
 
 
-  if (!GetModuleFileName(hInstance, moduleName, MAX_PATH))
-  {
+  if (!GetModuleFileName(hInstance, moduleName, MAX_PATH)) {
     MessageBox(0, "Failed calling GetModuleFileName()", title, type);
     return 1;
   }
 
-  if (_splitpath_s(moduleName, moduleRootDrive, _MAX_DRIVE, moduleRootDir, _MAX_DIR, NULL, 0, NULL, 0) != 0)
-  {
+  if (_splitpath_s(moduleName, moduleRootDrive, _MAX_DRIVE, moduleRootDir, _MAX_DIR, NULL, 0, NULL, 0) != 0) {
     MessageBox(0, "Failed calling _splitpath_s()", title, type);
     return 1;
   }
@@ -49,8 +104,7 @@ int APIENTRY WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLi
   dll = dirname + "\\bin\\steam_api.dll";
   hinstLauncher = LoadLibraryEx(dll.c_str(), NULL, LOAD_WITH_ALTERED_SEARCH_PATH);
 
-  if (!hinstLauncher)
-  {
+  if (!hinstLauncher) {
     char *errmsg;
 
     if (FormatMessage(FORMAT_MESSAGE_ALLOCATE_BUFFER | FORMAT_MESSAGE_FROM_SYSTEM | FORMAT_MESSAGE_IGNORE_INSERTS,
@@ -72,36 +126,26 @@ int APIENTRY WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLi
   pSteamAPI_IsSteamRunning = (SteamAPI_IsSteamRunning_t) GetProcAddress(hinstLauncher, "SteamAPI_IsSteamRunning");
 
 
-  /* try to launch Steam if nothing was found */
+  /* try to launch Steam if needed */
 
   if (!pSteamAPI_IsSteamRunning()) {
     STARTUPINFO si;
     PROCESS_INFORMATION pi;
     DWORD dwret;
-    std::string str, command;
-    const char *errmsg = "Unable to find a running instance of Steam.";
-    const char *steam_path;
+    std::string steam_path, command;
+    const char *errmsg;
 
     FreeLibrary(hinstLauncher);
 
-    str = dirname + "\\..\\..\\..\\Steam.exe";
-    steam_path = str.c_str();
+    steam_path = get_steam_path(dirname);
 
-    if (!PathFileExists(steam_path)) {
-      steam_path = "C:\\Program Files (x86)\\Steam\\Steam.exe";
-    }
-    if (!PathFileExists(steam_path)) {
-      steam_path = "C:\\Program Files\\Steam\\Steam.exe";
-    }
-    if (!PathFileExists(steam_path)) {
-      steam_path = "C:\\Steam\\Steam.exe";
-    }
-    if (!PathFileExists(steam_path)) {
-      MessageBox(0, errmsg, title, type);
+    if (steam_path == "") {
+      MessageBox(0, "Unable to find a running instance of Steam.", title, type);
       return 1;
     }
 
-    command = std::string(steam_path) + " -applaunch 362890";
+    command = steam_path + " -applaunch 362890";
+
     if (strlen(lpCmdLine) > 0) {
       command += " " + std::string(lpCmdLine);
     }
