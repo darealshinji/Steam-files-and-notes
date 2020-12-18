7630c1e01a83c1a51925d2e30b506c569c57f6be
darealshinji <djcj@gmx.de>
2020-12-18 08:07:05 +0100

    New game: Tiny Bunny

---
+++ Tiny Bunny/README.md
@@ -0,0 +1,5 @@
+AppID: `1249880`
+
+The script `tinybunny_unix.sh` will download the files needed to play the game
+natively on Linux and Mac.
+
---
+++ Tiny Bunny/tinybunny_unix.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+set -e
+set -x
+
+sdkver="7.3.5"
+
+mkdir -p "Tiny Bunny/lib"
+cd "Tiny Bunny"
+
+#wget https://www.renpy.org/dl/$sdkver/renpy-${sdkver}-sdk.tar.bz2
+wget https://archive.org/download/renpy-${sdkver}-sdk/renpy-${sdkver}-sdk.tar.bz2
+tar xf renpy-${sdkver}-sdk.tar.bz2
+
+# move files
+mv renpy-${sdkver}-sdk/renpy.sh TinyBunny.sh
+mv renpy-${sdkver}-sdk/lib/linux-i686 lib/
+mv renpy-${sdkver}-sdk/lib/linux-x86_64 lib/
+mv renpy-${sdkver}-sdk/lib/darwin-x86_64 lib/
+
+# rename binaries
+mv lib/linux-i686/renpy lib/linux-i686/TinyBunny
+mv lib/linux-x86_64/renpy lib/linux-x86_64/TinyBunny
+mv lib/darwin-x86_64/renpy lib/darwin-x86_64/TinyBunny
+
+# download SteamAPI libraries (taken from https://partner.steamgames.com/)
+wget -O lib/linux-i686/libsteam_api.so https://github.com/darealshinji/Steam-files-and-notes/raw/master/libsteam_api.so
+wget -O lib/linux-x86_64/libsteam_api.so https://github.com/darealshinji/Steam-files-and-notes/raw/master/libsteam_api64.so
+wget -O lib/darwin-x86_64/libsteam_api.dylib https://github.com/darealshinji/Steam-files-and-notes/raw/master/libsteam_api.dylib
+
+# delete
+rm renpy-${sdkver}-sdk.tar.bz2
+rm -rf renpy-${sdkver}-sdk
+
+
